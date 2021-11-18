7b5177697da8839bb620421d6ecbaf73b5b36524
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-11-17 19:53:20 -0500

    Initial commit

---
+++ src/test/java/com/careerdevs/gateway/gorest/GorestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.gateway.gorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
